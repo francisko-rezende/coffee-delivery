58e419b215685834845c99ccbbb08ea56b67d00e
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-11-09 09:37:57 -0300

    chore: setup prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -10,5 +11,6 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  { rules: { "react/react-in-jsx-scope": "off" } },
+  eslintConfigPrettier,
 ];
-
